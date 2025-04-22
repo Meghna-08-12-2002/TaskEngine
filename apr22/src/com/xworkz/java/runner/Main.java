5d0703f87cbdc551ec3cce304ec200197be21cfc
Meghna-08-12-2002 <ninganurmeghna@gmail.com>
2025-04-22 15:10:18 +0530

    commit

---
+++ apr22/src/com/xworkz/java/runner/Main.java
@@ -0,0 +1,11 @@
+package com.xworkz.java.runner;
+
+import com.xworkz.java.external.Btmroad;
+import com.xworkz.java.internal.rule.Traffic;
+
+public class Main {
+    public static void main(String[] args) {
+        Traffic traffic=new Btmroad();
+        traffic.road();
+    }
+}
