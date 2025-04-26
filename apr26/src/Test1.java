ebec62ae48fa3397f32ce6d238a4e894e18a5cd8
Meghna-08-12-2002 <ninganurmeghna@gmail.com>
2025-04-26 22:48:16 +0530

    commit

---
+++ apr26/src/Test1.java
@@ -0,0 +1,12 @@
+public class Test1 {
+
+    public static void main(String[] args) {
+        int x;
+    if(args.length>0){
+        x=10;
+    }else{
+        x=20;
+    }
+    System.out.println(x);
+    }
+}
