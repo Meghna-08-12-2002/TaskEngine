db55ea9ce8be66c431e683da2de983a700402af1
Meghna-08-12-2002 <ninganurmeghna@gmail.com>
2025-04-27 18:12:55 +0530

    commit

---
+++ apr27/src/Egg.java
@@ -0,0 +1,14 @@
+public class Egg {
+    public static void main(String[] args) {
+        int[] a={10,20,30};
+        int[] b={40,50,60};
+
+    }
+
+    public static void m1(int... x) {
+        for(int x1:x){
+         System.out.println(x);
+        }
+    }
+
+}
